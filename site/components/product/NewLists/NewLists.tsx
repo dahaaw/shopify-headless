331f4dac34bebe717c8a89c91c308cda93beed81
dahaaw <dahaaw@gmail.com>
2022-06-16 23:49:57 +0700

    fix types error

--- site/components/product/NewLists/NewLists.tsx
+++ site/components/product/NewLists/NewLists.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import { FC } from 'react'
 import { ProductCard } from '@components/product'
+import { Product } from '@commerce/types/product'
 
-export default function NewLists({ title, products = [] }) {
-  console.log({ products })
+interface Props {
+  title: string
+  products: Array<any>
+}
+
+const NewLists: FC<Props> = ({ title, products = [] }) => {
   return (
     <section className="bg-gray-100 py-12">
       <div className="container max-w-screen-xl mx-auto px-4">
         <h2 className="text-3xl font-bold mb-8">{title}</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {products.map((pdk) => (
+          {products.map((pdk: Product) => (
             <ProductCard product={pdk} />
           ))}
         </div>
@@ -16,3 +21,4 @@ export default function NewLists({ title, products = [] }) {
     </section>
   )
 }
+export default NewLists
